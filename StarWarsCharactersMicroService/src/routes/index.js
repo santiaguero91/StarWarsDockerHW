9faafb74aac0b8dc51a904e223115b2a8b6cabab
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-09-12 15:21:26 -0300

    creando pjs

--- StarWarsCharactersMicroService/src/routes/index.js
+++ StarWarsCharactersMicroService/src/routes/index.js
@@ -3,5 +3,6 @@ const controllers = require("../controllers")
 
 const router = Router();
 router.get("/",controllers.getCharacters)
+router.post("/",controllers.createCharacters)
 
 module.exports = router;
