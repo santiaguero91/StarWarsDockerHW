668572e3d3d102b8cbd9276734f2de7f35bfa62f
Santiago Nicolás Agüero Urquiza <santiagonaguero@gmail.com>
2023-09-18 10:33:10 -0300

    mongoDB started

---
+++ database/index.js
@@ -0,0 +1,6 @@
+const server = require("./src/server")
+
+server.listen(8004,()=>{
+    console.log("Darabase service on PORT 8004")
+})
+
--- gateway/index.js
+++ gateway/index.js
@@ -1,22 +1,31 @@
 const express = require("express");
-const morgan = require('morgan');
-const {createProxyMiddleware} = require("http-proxy-middleware")
+const morgan = require("morgan");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 app.use(morgan("dev"));
 
-app.use("/characters", createProxyMiddleware({
-    target:"http://localhost:3004",
-    changeOrigin:true
-}));
-app.use("/films", createProxyMiddleware({
-    target:"http://localhost:3003",
-    changeOrigin:true
-}));
-app.use("/planets", createProxyMiddleware({
-    target:"http://localhost:3002",
-    changeOrigin:true
-}));
-app.listen(3001,()=>{
-    console.log("Gateway on port 3001");
+app.use(
+  "/characters",
+  createProxyMiddleware({
+    target: "http://characters:3004",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/films",
+  createProxyMiddleware({
+    target: "http://films:3003",
+    changeOrigin: true,
+  })
+);
+app.use(
+  "/planets",
+  createProxyMiddleware({
+    target: "http://planets:3002",
+    changeOrigin: true,
+  })
+);
+app.listen(3001, () => {
+  console.log("Gateway on port 3001");
 });
